a808fc4ac1e31daf3ddf365c4fcf8e79498e6dc8
gavinsun <gavinsun@qq.com>
2019-06-14 09:50:51 +0800

    实现博客基本功能

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,26 +1,57 @@
 from django.shortcuts import render
+from blog.models import Article,Category
 
 # Create your views here.
 
-
 def home(request):
-    return render(request,template_name='blog/index2.html')
-
-
-# 显示所有文章
-def articles(request):
-    # TODO:获取所有文章
-    pass
+    """显示所有文章分类，和所有文章标题"""
+    categories = Category.objects.all()
+    posts = Article.objects.all()
+    c = {
+        'categories': categories,
+        'posts': posts
+    }
+    c
+    return render(request, template_name='blog/index3.html'
+                  , context=c)
 
 
 # 显示所有分类
 def category_list(request):
-    # TODO:获取所有分类列表
+    # 获取所有分类列表
+    # 获得所有用all
+    categories = Category.objects.all()
+    return render(request,template_name='blog/category.html'
+                  ,context={'categories':categories})
     # TODO: 使用Redis缓存分类列表，提高网站的访问速度
-    pass
+
+# 显示某分类下的所有文章
+def articles_by_category(request,category_id):
+    # 获得不确定个数的对象，用filter
+    posts = Article.objects.filter(category_id=category_id)
+    # print(posts.query)
+    categories = Category.objects.all()
+    c = {
+        'categories': categories,
+        'posts': posts
+    }
+
+    return render(request,template_name='blog/index3.html',context=c)
+
+# 显示所有文章
+def articles(request):
+    # TODO:获取所有文章
+    posts = Article.objects.all()
+    return render(request,template_name='blog/index3.html',context={'posts':posts})
+
 
 
 # 显示某个文章的详情
-def article_detail(request,pk):
-    # TODO:根据文章的ID，返回对应的内容
-    pass
+def article_detail(request,article_id):
+    # 获得某个用get
+    post = Article.objects.get(id=article_id)
+    return render(request, template_name='blog/detail.html',
+                  context={"post": post})
+
+
+# TODO: 分页
