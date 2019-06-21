5b5161079411510231f56d4cb2166885812a6b6a
gavinsun <gavinsun@qq.com>
2019-06-21 11:29:32 +0800

    测试用例编写

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,19 +1,50 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Article,Category
 
 # Create your views here.
 
-def home(request):
+def home(request,page=1):
     """显示所有文章分类，和所有文章标题"""
     categories = Category.objects.all()
-    posts = Article.objects.all()
+    posts = Article.objects.all()[:100]
+
     # posts = Article.objects.raw("select * from blog_article")
+
+    # 分页功能
+    # 1. 创建分页对象
+    paginator = Paginator(posts, 2)
+    # 2. 获得分页对象的总页数
+    num_pages = paginator.num_pages
+    # 3. 获取第page页数据
+    posts = paginator.page(page)
+    pages = range(1, 6)
+    page = int(page)
+    # 4. 页面范围控制
+
+    # 4.1.总页数<5, 显示所有页码   1 2 3 4 5
+
+    if num_pages < 5:
+        pages = range(1,6)
+    # 4.2.当前页是前3页，显示1-5页 1 2 3 4 5
+    elif page <=3:
+        pages = range(1,6)
+    # 4.3.当前页是后3页，显示后5页 6 7 8 9 10
+    elif page > num_pages - 3:
+        pages = range(num_pages-4,num_pages+1)  # 后5 页
+    # 4.4.其他情况，显示当前页前2页，后2页，当前页   10 11 12 13 14 15
+    else:
+        pages = range(page-2,page+3)  #  page = 6, 4 5 6 7 8  ,page放中间
+
+
     c = {
         'categories': categories,
-        'posts': posts
+        'posts': posts,
+        'pages': pages,
+        'cur_page':page,
     }
-    c
-    return render(request, template_name='blog/index3.html'
+
+    return render(request, template_name='blog/index.html'
                   , context=c)
 
 
